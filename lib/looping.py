feb3a2113404ca7a0fbde95a4b36bacd1214c60e
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-05-23 21:07:27 +0300

    Commit changes

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
+    return [int * int for int in int_list]
     pass
 
 def fizzbuzz():
     # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
     pass
